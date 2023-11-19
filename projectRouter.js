34576eaf023adb6b08d62a59f42b00e710d0c8c8
Tzvi-Setesman <148970900+Tzvi-Setesman@users.noreply.github.com>
2023-11-19 13:17:36 +0200

    fix add board

--- projectRouter.js
+++ projectRouter.js
@@ -1,124 +1,125 @@
 // routes/projectRouter.js
-import express  from 'express';
+import express from 'express';
 import mongoose from 'mongoose';
 import Project from './project.js';
-import ProjectNames from './listProjects.js';
-
 
 const router = express.Router();
-
-
 
 router.post('/addNewProject', async (req, res) => {
   const { name } = req.body;
   try {
-    // Assuming 'findProject' returns the appropriate model for the project
-    const newProject = new Project({ name, items: [] });
+    const newProject = new Project({
+      projectName: name,
+      pic: '',
+      man_in_charge: '',
+      header: '',
+      content: '',
+      deadline: '',
+      status: '',
+      category: '',
+      assignee: '',
+      milestone: '',
+      issue_type: ''
+    });
     const savedProject = await newProject.save();
     res.status(201).json(savedProject);
   } catch (error) {
     console.error('Error adding new project:', error);
     res.status(500).json({ error: 'Internal Server Error', details: error.message });
   }
-
 });
 
-
-// get request to send the all boards exsisting 
 router.get('/listOfProjects', async (req, res) => {
   try {
-    const list = await ProjectNames.find()
-    res.send(list.map((itm)=>itm.name))
+    const list = await Project.find();
+    res.send(list.map((itm) => itm.projectName));
   } catch (error) {
     console.log('Error getting list of project names:', error);
     res.status(500).json({ error: 'Internal Server Error', details: error.message });
   }
 });
-// get request to get all names exsist
 
-router.get('/names',async (req,res)=>{
-  try{
-  const response = await Project.find()
-  const names = [... new Set(response.map((obj) => ({ id: obj._id, name: obj.assignee })))]
-  res.send(names)
-  }catch(err){console.log('error while trying to get names ',(err));
-res.status(500).json({err:'interval server error',details:err.message})}
-})
-// post request to delete mission
-router.post('/delete/:projectName',async(req,res)=>{
-  const {id} = req.body
-  const projName = req.params.projectName
-  try{
-  if (!id || !projName) {
-    return res.status(400).json({ error: 'Missing required parameters.' });
+router.get('/names', async (req, res) => {
+  try {
+    const response = await Project.find();
+    const names = [...new Set(response.map((obj) => ({ id: obj._id, name: obj.assignee })))];
+    res.send(names);
+  } catch (err) {
+    console.log('error while trying to get names ', err);
+    res.status(500).json({ err: 'Internal Server Error', details: err.message });
   }
-  const missionToDelete = await Project.findOne({ _id: id, projectName: projName });
-  if (!missionToDelete) {
-    return res.status(404).json({ error: 'Mission not found.' });
+});
+
+router.post('/delete/:projectName', async (req, res) => {
+  const { id } = req.body;
+  const projName = req.params.projectName;
+  try {
+    if (!id || !projName) {
+      return res.status(400).json({ error: 'Missing required parameters.' });
+    }
+    const missionToDelete = await Project.findOne({ _id: id, projectName: projName });
+    if (!missionToDelete) {
+      return res.status(404).json({ error: 'Mission not found.' });
+    }
+    await missionToDelete.deleteOne();
+    return res.status(200).json({ message: 'Mission deleted successfully.' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal Server Error.' });
   }
-  await missionToDelete.deleteOne();
-  return res.status(200).json({ message: 'Mission deleted successfully.' });
-} catch (error) {
-  console.error(error);
-  return res.status(500).json({ error: 'Internal Server Error.' });
-}
-})
+});
 
-
-// post request to add new mission 
-router.post('/addMission/:collection',async(req,res)=>{
-  const collection = req.params
-  const newMission = req.body
-  newMission['projectName']=collection.collection
-try{
-   const mission = new Project(newMission)
-   const savedMission = await mission.save();
-
+router.post('/addMission/:collection', async (req, res) => {
+  const collection = req.params;
+  const newMission = req.body;
+  newMission['projectName'] = collection.collection;
+  try {
+    const mission = new Project(newMission);
+    const savedMission = await mission.save();
     res.json(savedMission);
   } catch (error) {
     console.error('Error adding mission:', error);
     res.status(500).json({ error: 'Internal Server Error', details: error.message });
   }
+});
 
-})
-
-
-// post request to update mission fields
 router.post('/post/:collectionName/:field', async (req, res) => {
   const { collectionName, field } = req.params;
   const { id, update } = req.body;
-try{
-  const current = await Project.findOne({_id:id,projectName:collectionName})
-  const updatedMission = await Project.findOneAndUpdate(
-    { _id:id,projectName:collectionName},
-    { $set: { [field]: update} },
-    { new: true } 
-  );
-  if (current[field]===updatedMission[field]){res.send(updatedMission[field])}
-  else{
-  if (!updatedMission) {
-    return res.status(404).json({ error: 'Mission not found', details: `No mission with the specified id` });
+  try {
+    const current = await Project.findOne({ _id: id, projectName: collectionName });
+    const updatedMission = await Project.findOneAndUpdate(
+      { _id: id, projectName: collectionName },
+      { $set: { [field]: update } },
+      { new: true }
+    );
+    if (current[field] === updatedMission[field]) {
+      res.send(updatedMission[field]);
+    } else {
+      if (!updatedMission) {
+        return res.status(404).json({ error: 'Mission not found', details: `No mission with the specified id` });
+      }
+      res.json(updatedMission);
+    }
+  } catch (err) {
+    console.log('error while update mission', err);
   }
-  res.json(updatedMission);}
-}catch(err){console.log('error while update mission',(err));}
-})
+});
 
-
-// get request to get all the missions by board name
 router.get('/:projectName', async (req, res) => {
   const { projectName } = req.params;
 
   try {
-        const query = await Project.find({ projectName: projectName });
-        if (!query) {
-          res.send('No project found');
-        } else {
-          res.send(query);
-        }
-      } catch (err) {
-        console.log('Error:', err);
-        res.status(500).send('Internal Server Error');
-      }
+    const query = await Project.find({ projectName: projectName });
+    if (!query) {
+      res.send('No project found');
+    } else {
+      res.send(query);
+    }
+  } catch (err) {
+    console.log('Error:', err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 export default router;
