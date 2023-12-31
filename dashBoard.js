9296dfda0e27a07edeaa738bd4ed68684379f2a0
didi9762 <yedidya9762@gmail.com>
2023-12-31 13:35:08 +0200

    fix deshboard request

--- dashBoard.js
+++ dashBoard.js
@@ -7,7 +7,9 @@ const deshRouter = express.Router();
 
 deshRouter.get('/tasks',async(req,res)=>{
     const  enecodeName= req.headers.authorization
+    console.log('e:',enecodeName);
     const userName = decodeURIComponent(atob(enecodeName))
+    console.log('user:',userName);
     if(!userName){res.status(500).send('no user name found');return}
     try{
         const listTasks = await Project.find({'assignee.userName':userName})
@@ -19,9 +21,9 @@ deshRouter.get('/tasks',async(req,res)=>{
           };
           listTasks.forEach((task) => {
             const { status } = task;
-            if (status === 'Not started') {
+            if (status === 'Not Started') {
               responseObject.n += 1;
-            } else if (status === 'In progress') {
+            } else if (status === 'In Progress') {
               responseObject.i += 1;
             } else if (status === 'Completed') {
               responseObject.c += 1;
